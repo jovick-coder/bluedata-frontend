2c6ee5d466af01dcd3146542272d2ad20bb3b041
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-09 21:40:01 +0000

    nav list

---
+++ src/components/NavBar/dashboradNavList.js
@@ -0,0 +1,19 @@
+import { BsFillHddStackFill, BsHouseFill, BsPerson } from "react-icons/bs";
+
+export const navLinkObject = [
+  {
+    name: "Home",
+    icon: <BsHouseFill />,
+    path: "/dashboard/home",
+  },
+  {
+    name: "profile",
+    icon: <BsPerson />,
+    path: "/dashboard/profile",
+  },
+  {
+    name: "History",
+    icon: <BsFillHddStackFill />,
+    path: "/dashboard/history",
+  },
+];
