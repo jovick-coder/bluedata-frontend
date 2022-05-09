fb6699820a9e127df7591ddaf928a69661981c08
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-09 21:37:47 +0000

    added new routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 import NavBarComponent from "./components/NavBar/NavBarComponent.jsx";
 import { useContext, useState } from "react";
-import { ThemesContext, ThemesProvider } from "./context/themesContext";
+import { ThemesContext } from "./context/themesContext";
 import ThemeTogglerComponent from "./components/ThemeToggler/ThemeTogglerComponent";
 import LoginPage from "./pages/Loginpage/LoginPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import HomePage from "./pages/Home/HomePage";
+import ErrorPage from "./pages/error_page/error_page.component";
+import ProfilePage from "./pages/Profile/ProfilePage";
+import HistoryPage from "./pages/History/HistoryPage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   // const [isError, setIsError] = useState({ error: false, message: "" });
-  const [theme, setTheme] = useContext(ThemesContext);
+  const [theme] = useContext(ThemesContext);
   return (
     <div className="App" data-theme={theme} s>
       <NavBarComponent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
@@ -23,12 +23,16 @@ function App() {
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        {/* 
-    <Route path="/404" element={<NotFound />} />
-              <Route path="*" element={<Navigate replace to="/404" />} /> */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="home" element={<HomePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="history" element={<HistoryPage />} />
+        </Route>
+
+        <Route path="/404" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
       </Routes>
     </div>
-    // </ThemesProvider>
   );
 }
 
