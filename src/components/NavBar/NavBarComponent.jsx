ceca36789f31ae5c8eadfc559cb8e485fad3099b
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-09 21:49:32 +0000

    side nav

--- src/components/NavBar/NavBarComponent.jsx
+++ src/components/NavBar/NavBarComponent.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./NavBarComponent.css";
 import profilePicture from "../../assets/images/profilePicture.jpg";
-import { BsCaretDownFill } from "react-icons/bs";
+import {
+  BsArrowLeftSquareFill,
+  BsArrowRightSquareFill,
+  BsCaretDownFill,
+  BsFillDoorOpenFill,
+} from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
-import { SignInForm } from "../Forms/FormsComponent";
+import { LoginForm } from "../Forms/FormsComponent";
+import { UserContext } from "../../context/userContext";
+import { navLinkObject } from "./dashboradNavList";
+function NavBarComponent() {
+  // function handelLogin() {
+  //   // confirm("Logout?") navigate("/");
+  //   // window.confirm("login?") && setLoggedIn(true);
 
-function NavBarComponent({ isLoggedIn, setIsLoggedIn }) {
-  function handelLogin() {
-    // confirm("Logout?") navigate("/");
-    // window.confirm("login?") && setIsLoggedIn(true);
-
-    window.document.querySelector(".LoginComponent").style.border = "solid red";
-  }
+  //   window.document.querySelector(".LoginComponent").style.border = "solid red";
+  // }
+  const { loggedIn, setLoggedIn } = useContext(UserContext);
   const [loginCardIsOpen, setLoginCardIsOpen] = useState(false);
   const [signInCardIsOpen, setSignInCardIsOpen] = useState(false);
   return (
     <div className="NavBarComponent">
       <div className="container mx-auto d-flex justify-content-between ">
         <LogoComponent />
-        {isLoggedIn ? (
-          <NavMenuComponent setIsLoggedIn={setIsLoggedIn} />
+        {loggedIn ? (
+          <NavMenuComponent setLoggedIn={setLoggedIn} />
         ) : (
           <div className="d-flex drop-down-div">
             {/* <Link className="my-auto mx-3" to="/">
@@ -61,7 +68,9 @@ function NavBarComponent({ isLoggedIn, setIsLoggedIn }) {
                   : null
               }
             >
-              <SignInForm />
+              <div className="mt-5">
+                <LoginForm />
+              </div>
             </div>
             <div
               className={`drop-down-card `}
@@ -87,9 +96,11 @@ export const LogoComponent = () => {
 };
 
 export const NavProfilePicture = () => {
+  const { userInformation } = useContext(UserContext);
+
   return (
     <div className="ms-auto my-auto">
-      <b>UserName</b>
+      <b> {userInformation.userName}</b>
       <img
         src={profilePicture}
         className="NavProfilePicture ms-2"
@@ -103,11 +114,11 @@ export const NavLinkComponent = ({ linkText, to }) => {
   return <div className="NavLinkComponent">{linkText} </div>;
 };
 
-export const NavMenuComponent = ({ setIsLoggedIn }) => {
+export const NavMenuComponent = ({ setLoggedIn }) => {
   const [navMenuComponent, setNavMenuComponent] = useState(false);
   const navigate = useNavigate();
   function logout() {
-    window.confirm("logout?") && setIsLoggedIn(false);
+    window.confirm("logout?") && setLoggedIn(false);
     navigate("/");
   }
   return (
@@ -142,12 +153,55 @@ export const NavMenuComponent = ({ setIsLoggedIn }) => {
   );
 };
 
-export const ProfilePicture = () => {
+export const ProfilePicture = ({ className }) => {
   return (
     <img
       src={profilePicture}
-      className="ProfilePicture ms-2"
+      className={`ProfilePicture ms-2 ${className}`}
       alt="ProfilePicture"
     />
+  );
+};
+
+export const DashboardSideNav = ({ sideNavOpen, setSideNavOpen }) => {
+  // const [sideNavOpen, setSideNavOpen] = useState(false);
+  function navToggle() {
+    setSideNavOpen(!sideNavOpen);
+  }
+
+  const { logOut } = useContext(UserContext);
+  return (
+    <>
+      {/* <div className="btn btn-primary"> open</div> */}
+      <div
+        className="side-nav-icon toggle"
+        onClick={() => {
+          navToggle();
+        }}
+      >
+        {sideNavOpen ? <BsArrowLeftSquareFill /> : <BsArrowRightSquareFill />}
+      </div>
+      <ul>
+        {navLinkObject.map((link, i) => {
+          const { name, icon, path } = link;
+          return (
+            <li key={i}>
+              <Link to={path}>
+                <div className="side-nav-icon">{icon}</div>
+                <span className="nav-link-name">{name}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <div
+        className="side-nav-icon toggle text-danger"
+        onClick={() => {
+          logOut();
+        }}
+      >
+        <BsFillDoorOpenFill />
+      </div>
+    </>
   );
 };
