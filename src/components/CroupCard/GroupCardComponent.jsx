82f535452ce9f82dddaedeef59bbea7af05ca1fb
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-09 21:38:08 +0000

    GroupCardComponent

---
+++ src/components/CroupCard/GroupCardComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './GroupCardComponent.css'
+
+const GroupCard = ({ children }) => {
+  return <div className="main-card">{children}</div>
+}
+
+export default GroupCard
