04f74bb0a48d5bbdea01d34e4309cad72902a4eb
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-11 15:53:45 +0000

    handel submite sub

--- src/pages/Subscriptions/SubscriptionsPage.jsx
+++ src/pages/Subscriptions/SubscriptionsPage.jsx
@@ -21,37 +21,37 @@ function SubscriptionsPage() {
   useEffect(() => {
     if (network === "MTN") {
       setDataPlan([
-        { plan: "MTN 500MB @150", value: "500MB" },
-        { plan: "MTN 1GB @300", value: "1GB" },
-        { plan: "MTN 2GB @600", value: "2GB" },
-        { plan: "MTN 3GB @1200", value: "3GB" },
+        { plan: "MTN 500MB @150", value: "MTN 500MB" },
+        { plan: "MTN 1GB @300", value: "MTN 1GB" },
+        { plan: "MTN 2GB @600", value: "MTN 2GB" },
+        { plan: "MTN 3GB @1200", value: "MTN 3GB" },
       ]);
       return;
     }
     if (network === "GLO") {
       setDataPlan([
-        { plan: "GLO 500MB @200", value: "500MB" },
-        { plan: "GLO 1GB @400", value: "1GB" },
-        { plan: "GLO 2GB @800", value: "2GB" },
-        { plan: "GLO 3GB @1400", value: "3GB" },
+        { plan: "GLO 500MB @200", value: "GLO 500MB" },
+        { plan: "GLO 1GB @400", value: "GLO 1GB" },
+        { plan: "GLO 2GB @800", value: "GLO 2GB" },
+        { plan: "GLO 3GB @1400", value: "GLO 3GB" },
       ]);
       return;
     }
     if (network === "Artel") {
       setDataPlan([
-        { plan: "Artel 500MB @500", value: "500MB" },
-        { plan: "Artel 1GB @1000", value: "1GB" },
-        { plan: "Artel 2GB @2000", value: "2GB" },
-        { plan: "Artel 3GB @3000", value: "3GB" },
+        { plan: "Artel 500MB @500", value: "Artel 500MB" },
+        { plan: "Artel 1GB @1000", value: "Artel 1GB" },
+        { plan: "Artel 2GB @2000", value: "Artel 2GB" },
+        { plan: "Artel 3GB @3000", value: "Artel 3GB" },
       ]);
       return;
     }
     if (network === "9Mobile") {
       setDataPlan([
-        { plan: "9Mobile 500MB @550", value: "500MB" },
-        { plan: "9Mobile 1GB @1200", value: "1GB" },
-        { plan: "9Mobile 2GB @2400", value: "2GB" },
-        { plan: "9Mobile 3GB @3600", value: "3GB" },
+        { plan: "9Mobile 500MB @550", value: "9mobile 500MB" },
+        { plan: "9Mobile 1GB @1200", value: "9mobile 1GB" },
+        { plan: "9Mobile 2GB @2400", value: "9mobile 2GB" },
+        { plan: "9Mobile 3GB @3600", value: "9mobile 3GB" },
       ]);
       return;
     }
@@ -64,7 +64,39 @@ function SubscriptionsPage() {
     // console.log(network);
     setNetwork(network);
   }
-  function handelSubmit(e) {}
+  function handelSubmit(e) {
+    e.preventDefault();
+    const formElement = e.target;
+
+    if (formElement[0].value === "") {
+      formElement[0].style.border = "solid red 1px";
+      setFormError({ error: true, message: "Network is not selected" });
+      return;
+    }
+    formElement[0].style.border = "solid #ddd 1px";
+    if (formElement[1].value === "") {
+      formElement[1].style.border = "solid red 1px";
+      setFormError({ error: true, message: "Number is empty" });
+      return;
+    }
+    formElement[1].style.border = "solid #ddd 1px";
+    if (formElement[2].value === "") {
+      formElement[2].style.border = "solid red 1px";
+      setFormError({ error: true, message: "Amount is empty" });
+      return;
+    }
+    setFormError({ error: false, message: "" });
+    formElement[2].style.border = "solid #ddd 1px";
+    const sendBody = {
+      plan: formElement[0].value,
+      Number: parseInt(formElement[1].value),
+      amount: parseInt(formElement[2].value),
+      method: "-",
+    };
+    console.log(sendBody);
+    // formElement[3].innerText = "Sending...";
+    // formElement[3].setAttribute("disabled", true);
+  }
 
   return (
     <div className="FoundAccountPage mt-5">
