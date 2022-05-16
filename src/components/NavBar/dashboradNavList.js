3b1b780662863ed5192ea88304d0330dfd3818e6
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-16 14:36:26 +0000

    adda more nav list and added privilege to each

--- src/components/NavBar/dashboradNavList.js
+++ src/components/NavBar/dashboradNavList.js
@@ -3,39 +3,138 @@ import {
   BsCoin,
   BsFillHddStackFill,
   BsHouseFill,
+  BsPeople,
   BsPerson,
+  BsPersonBadge,
+  BsPersonCheck,
+  BsPersonPlus,
+  BsPersonX,
   BsSimFill,
 } from "react-icons/bs";
 
-export const navLinkObject = [
+export const userNavLinkObject = [
   {
     name: "Home",
     icon: <BsHouseFill />,
     path: "/dashboard/home",
+    userPrivilege: 1,
   },
   {
     name: "profile",
     icon: <BsPerson />,
     path: "/dashboard/profile",
+    userPrivilege: 1,
   },
   {
     name: "History",
     icon: <BsFillHddStackFill />,
     path: "/dashboard/history",
+    userPrivilege: 1,
   },
   {
     name: "Notification",
     icon: <BsBellFill />,
     path: "/dashboard/notification",
+    userPrivilege: 1,
   },
   {
     name: "Found Account",
     icon: <BsCoin />,
     path: "/dashboard/Found-account",
+    userPrivilege: 1,
   },
   {
     name: "Buy Subscriptions",
     icon: <BsSimFill />,
     path: "/dashboard/Subscriptions",
+    userPrivilege: 1,
+  },
+  {
+    name: "Customers List",
+    icon: <BsPersonCheck />,
+    path: "/dashboard/customers-list",
+    userPrivilege: 2,
+  },
+  {
+    name: "Users List",
+    icon: <BsPeople />,
+    path: "/dashboard/user-list",
+    userPrivilege: 3,
+  },
+  {
+    name: "Resellers List",
+    icon: <BsPersonPlus />,
+    path: "/dashboard/resellers-list",
+    userPrivilege: 3,
+  },
+  {
+    name: "Admins List",
+    icon: <BsPeople />,
+    path: "/dashboard/admin-list",
+    userPrivilege: 4,
   },
 ];
+// export const superAdminNavLinkObject = [
+//   // {
+//   //   name: "Home",
+//   //   icon: <BsHouseFill />,
+//   //   path: "/adminDashboard/home",
+//   // },
+//   {
+//     name: "profile",
+//     icon: <BsPerson />,
+//     path: "/adminDashboard/profile",
+//   },
+//   // {
+//   //   name: "History",
+//   //   icon: <BsFillHddStackFill />,
+//   //   path: "/adminDashboard/history",
+//   // },
+//   // {
+//   //   name: "Notification",
+//   //   icon: <BsBellFill />,
+//   //   path: "/adminDashboard/notification",
+//   // },
+//   // {
+//   //   name: "Found Account",
+//   //   icon: <BsCoin />,
+//   //   path: "/adminDashboard/Found-account",
+//   // },
+//   // {
+//   //   name: "Buy Subscriptions",
+//   //   icon: <BsSimFill />,
+//   //   path: "/adminDashboard/Subscriptions",
+//   // },
+// ];
+// export const adminNavLinkObject = [
+//   // {
+//   //   name: "Home",
+//   //   icon: <BsHouseFill />,
+//   //   path: "/adminDashboard/home",
+//   // },
+//   {
+//     name: "profile",
+//     icon: <BsPerson />,
+//     path: "/SuperAdminDashboard/profile",
+//   },
+//   // {
+//   //   name: "History",
+//   //   icon: <BsFillHddStackFill />,
+//   //   path: "/adminDashboard/history",
+//   // },
+//   // {
+//   //   name: "Notification",
+//   //   icon: <BsBellFill />,
+//   //   path: "/adminDashboard/notification",
+//   // },
+//   // {
+//   //   name: "Found Account",
+//   //   icon: <BsCoin />,
+//   //   path: "/adminDashboard/Found-account",
+//   // },
+//   // {
+//   //   name: "Buy Subscriptions",
+//   //   icon: <BsSimFill />,
+//   //   path: "/adminDashboard/Subscriptions",
+//   // },
+// ];
