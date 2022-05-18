a2eccfb7653081f5e2e5deae57830cfd760b57c8
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-18 08:11:25 +0000

    pop up message component and context

---
+++ src/components/PopUpMessage/PopUpMessageComponent.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from "react";
+import { BsX } from "react-icons/bs";
+import "./PopUpMessageComponent.css";
+import { PopUpMessageContext } from "../../context/PopUpMessageContext";
+
+function PopUpMessageComponent() {
+  const { popUpMessage, setPopUpMessage } = useContext(PopUpMessageContext);
+
+  return (
+    <div
+      className={`PopUpMessageComponent ${popUpMessage.messageType}`}
+      style={popUpMessage.messageType !== "" ? null : { display: "none" }}
+    >
+      <div className="w-100">{popUpMessage.message}</div>
+      <div className="close">
+        <BsX
+          onClick={() => setPopUpMessage({ messageType: "", message: "" })}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PopUpMessageComponent;
---
+++ src/context/PopUpMessageContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const PopUpMessageContext = createContext();
+
+export function PopUpMessageProvider({ children }) {
+  const [popUpMessage, setPopUpMessage] = useState({
+    messageType: "",
+    message: "",
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPopUpMessage({ messageType: "", message: "" });
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [popUpMessage]);
+
+  return (
+    <PopUpMessageContext.Provider value={{ popUpMessage, setPopUpMessage }}>
+      {children}
+    </PopUpMessageContext.Provider>
+  );
+}
