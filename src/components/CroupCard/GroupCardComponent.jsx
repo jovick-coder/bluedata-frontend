f4405c585aa23b2b10ffb105a92508895689d78b
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-21 15:28:19 +0000

    DashboardCard and DashboardCard design

--- src/components/CroupCard/GroupCardComponent.jsx
+++ src/components/CroupCard/GroupCardComponent.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import './GroupCardComponent.css'
+import React from "react";
+import "./GroupCardComponent.css";
+import { BsWallet } from "react-icons/bs";
 
 const GroupCard = ({ children }) => {
-  return <div className="main-card">{children}</div>
-}
+  return <div className="main-card">{children}</div>;
+};
 
-export default GroupCard
+export default GroupCard;
+
+export function DashboardCard({ icon, label, figure }) {
+  return (
+    <div className="DashboardCard">
+      <div className="icon-div">{icon}</div>
+      <div className="label-div">
+        {label}
+        <div className="figure-div">{figure}</div>
+      </div>
+    </div>
+  );
+}
+export function DashboardActionCard({ icon, label, figure }) {
+  return (
+    <div className="DashboardActionCard">
+      <div className="icon-div">{icon}</div>
+      <div className="label-div">
+        {label}
+        <div className="figure-div">{figure}</div>
+      </div>
+    </div>
+  );
+}
