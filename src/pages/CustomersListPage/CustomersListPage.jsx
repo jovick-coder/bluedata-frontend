527468db33ccb0ef843866af6e70f70cc877e011
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-02 10:58:05 +0000

    add a buton to pop up model

--- src/pages/CustomersListPage/CustomersListPage.jsx
+++ src/pages/CustomersListPage/CustomersListPage.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import { BsFillExclamationTriangleFill } from "react-icons/bs";
+import React, { useContext } from "react";
+import {
+  BsFillExclamationTriangleFill,
+  BsPlusCircleFill,
+} from "react-icons/bs";
 import GroupCard from "../../components/CroupCard/GroupCardComponent";
+import ModalComponent from "../../components/Modal/ModalComponent";
+import { UserContext } from "../../context/userContext";
 
 function CustomersListPage() {
   // Check privilege before showing page content
-  const token = localStorage.getItem("telecomMerchantToken");
-  const tokenArray = token.split(".");
-  const decode = JSON.parse(atob(tokenArray[1]));
-  if (decode.privilege < 2) {
+  // const token = localStorage.getItem("telecomMerchantToken");
+  // const tokenArray = token.split(".");
+  // const decode = JSON.parse(atob(tokenArray[1]));
+  const { apiUrl, getUserPrivilege } = useContext(UserContext);
+
+  if (getUserPrivilege() < 2) {
     return (
       <div className="mt-5 text-center ">
         <GroupCard>
@@ -25,7 +32,30 @@ function CustomersListPage() {
   }
   return (
     <div>
-      <GroupCard>CustomersListPage</GroupCard>
+      <GroupCard>
+        {getUserPrivilege() >= 2 ? (
+          <>
+            <ModalComponent modalTitle="Add Note" modalId={"AddCustomers"}>
+              Add Customer
+            </ModalComponent>
+            <div className="header d-flex justify-content-between">
+              {/* <h3>New Customer</h3> */}
+              <button
+                className="button "
+                // onClick={() => markHasSeen("*")}
+                // className="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target="#AddCustomers"
+              >
+                {" "}
+                <BsPlusCircleFill className="me-2 fs-5 my-auto" />
+                Add Customer
+              </button>
+            </div>
+            <hr />
+          </>
+        ) : null}
+      </GroupCard>
     </div>
   );
 }
